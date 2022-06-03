065644eafae42a7da0a361c3fe8f9af80302c03b
ChillFish8 <hburt2003@gmail.com>
2022-06-03 11:08:20 +0100

    Update lossless handling

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,11 @@ fn main() -> Result<()> {
     let mut args = std::env::args();
     let _ = args.next();
     let glob_arg = args.next().unwrap_or_else(|| "**/*.png".to_string());
+    let lossless = args.next().map(|v| v == "lossless").unwrap_or(false);
 
     for entry in glob::glob(&glob_arg)? {
         match entry {
-            Ok(entry) => optimise_image(&entry)?,
+            Ok(entry) => optimise_image(lossless, &entry)?,
             Err(e) => {
                 eprintln!("Skipping file due to error {:?}", e);
             }
@@ -21,13 +22,18 @@ fn main() -> Result<()> {
 }
 
 
-fn optimise_image(path: &Path) -> Result<()> {
+fn optimise_image(lossless: bool, path: &Path) -> Result<()> {
     let img = ImageReader::open(path)?.decode()?;
 
     let encoder: Encoder = Encoder::from_image(&img)
         .map_err(|e| anyhow!("{}", e))?;
 
-    let webp: WebPMemory = encoder.encode(90f32);
+    let webp: WebPMemory = if !lossless {
+        encoder.encode(90f32)
+    } else {
+        encoder.encode_lossless()
+    };
+
     let output_path = path.with_extension("webp");
     std::fs::write(&output_path, &*webp)?;
 
